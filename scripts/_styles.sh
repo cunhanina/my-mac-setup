07a84ee9e2e66a7d1d165a55e73deb9ab5c35897
maxykoin <stayerim@gmail.com>
2025-12-30 00:02:43 -0300

    feat: better file structure on newpy, better pyclean, more info in health

--- scripts/_styles.sh
+++ scripts/_styles.sh
@@ -7,6 +7,7 @@ YELLOW=$(tput setaf 3)
 BLUE=$(tput setaf 4)
 CYAN=$(tput setaf 6)
 RED=$(tput setaf 1)
+MAGENTA=$(tput setaf 5)
 
 # Headers (Cyan and Bold)
 header() { echo -e "\n${BOLD}${CYAN}➜ $1${RESET}"; }
@@ -18,3 +19,9 @@ row() { printf "  %-20s %s\n" "$1" "$2"; }
 ok() { echo -e "  ${GREEN}✔${RESET} $1"; }
 warn() { echo -e "  ${YELLOW}${RESET} $1"; }
 err() { echo -e "  ${RED}✖${RESET} $1"; }
+
+# NEW: Info (Blue "i" icon)
+info() { echo -e "  ${BLUE}ℹ️${RESET}  $1"; }
+
+# NEW: Step (Magenta arrow)
+step() { echo -e "  ${MAGENTA}➜${RESET} $1"; }
